2a1e5da20383bd177d3e62b7097652f9f7a560f6
turnwrighthere <j.a.wright2010@gmail.com>
2018-04-16 17:54:34 -0500

    Starting basic setup

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:build-a-blog@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run()
